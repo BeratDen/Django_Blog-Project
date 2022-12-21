3b7128a28eca16b2efdd2423c3132867053812ba
Berat Deniz <berat.denizsids@gmail.com>
2022-12-22 00:02:49 +0300

    Update

--- article/urls.py
+++ article/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 app_name = "article"
 
 urlpatterns = [
-    path("create/", views.index, name="index")
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("add/", views.add, name="add"),
+    path("article/<int:id>/", views.detail, name="article"),
+    path("update/<int:id>/", views.update, name="update"),
+    path("delete/<int:id>/", views.delete, name="delete"),
 ]
